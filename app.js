61ae8870b9fd9aaa73a6793c5b568221e913946a
Aleksander Velozo Pascoal <avelozo@sof.intra>
2018-07-17 08:58:31 -0300

    Incluindo boas vindas

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 process.env["NTBA_FIX_319"] = 1; //para tirar aviso de erro ao carregar node-telegram
 var TelegramBot = require('node-telegram-bot-api');
-const token = '';
-const bot = new TelegramBot(token, {polling: true});
+const token = '686115106:AAEwYAqij0Ar8eB1GO8X2Gj40PoI6gTgVhQ';
+const bot = new TelegramBot(token, {filepath: false, polling: true});
 
 var consign = require('consign');
 consign().include('service').into(bot);
 
 bot.onText(/\/buscar (.+)/, async (msg, match) => { 
-    console.log("onText");
     const chatId = msg.chat.id;
     const termoABuscar = match[1]; 
 
@@ -15,12 +14,16 @@ bot.onText(/\/buscar (.+)/, async (msg, match) => {
     let paginas = await intraSOF.buscar(termoABuscar);
 
     if (paginas && paginas.length > 0){
-        bot.sendMessage(chatId, "Encontrados " + paginas.length + " resultados");
+        await bot.sendMessage(chatId, "Encontrados " + paginas.length + " resultados");
         paginas.forEach(pagina => {bot.sendMessage(chatId, pagina)});
     }
     else{
         bot.sendMessage(chatId, "Nenhuma página encontrada");
     }        
+  });
+
+  bot.onText(/\/start/, async (msg, match) => { 
+    bot.sendMessage(msg.chat.id, "Olá, " + msg.from.first_name + " para buscar termos na IntraSOF use /buscar <termo>");
   });
 
 
